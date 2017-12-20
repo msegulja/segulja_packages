d0b2a6c3b68f565362257d62130ccc1ea2ebe849
Michael Segulja <michael@segulja.com>
2017-12-20 12:42:03 -0600

    Changes:
      - Install the epel-release repository
      - Install the ovirt-guest-agent-common packages
      - Start and enable the ovirt-guest-agent service
      - Version 0.1.1
    
    	modified:   .kitchen.yml
    	modified:   metadata.rb
    	modified:   recipes/default.rb
    	modified:   recipes/ovirt.rb
    	modified:   test/smoke/default/default_test.rb
    	modified:   test/smoke/default/ovirt_test.rb

--- recipes/ovirt.rb
+++ recipes/ovirt.rb
@@ -3,3 +3,19 @@
 # Recipe:: ovirt
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Install epel-release rpm repository
+# Install the ovirt-guest-agent rpm package
+# Start and enable the ovirt-guest-agent service
+
+package 'epel-release' do
+  action :install
+end
+
+package 'ovirt-guest-agent' do
+  action :install
+end
+
+service 'ovirt-guest-agent' do
+  action [:enable, :start]
+end
--- test/smoke/default/ovirt_test.rb
+++ test/smoke/default/ovirt_test.rb
@@ -5,14 +5,18 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# Is the epel-release package installed?
+# Is the ovirt-guest-agent package installed?
+# Is the ovirt-guest-agent service started and enabled?
+
+describe package('epel-release') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package('ovirt-guest-agent-common') do
+  it { should be_installed }
+end
+
+describe service('ovirt-guest-agent') do
+  it { should be_enabled }
 end
